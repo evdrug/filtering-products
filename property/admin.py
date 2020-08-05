6bc9eddfee55538423e1ee9495332fc9b8f84e3e
druzyakin.e <druzyakin.e@gmail.com>
2020-08-05 21:26:44 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/admin.py
+++ property/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from .models import Flat, Complaint
 
 # admin.site.register(Flat)
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = 'town', 'address', 'owner__contains',
     readonly_fields = 'created_at',
-    list_display = 'address', 'price', 'new_building', 'construction_year',
+    list_display = ('address', 'price', 'new_building',
+                    'construction_year', 'owners_phonenumber',
+                    'owner_phone_pure')
     list_editable = 'new_building',
     list_filter = 'new_building', 'rooms_number', 'has_balcony',
     raw_id_fields = "liked_by",
